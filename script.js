aa81582a57a0438ad3a814a8fa6b4de1a392f514
NatoDeveloper <renwise2.0@gmail.com>
2022-04-24 21:59:51 -0300

    Projeto finalizado com funções javascript

---
+++ script.js
@@ -0,0 +1,23 @@
+var Email = document.getElementById('Email');
+
+Email.addEventListener('focus',() => {
+    Email.style.borderColor = "#rgb(255, 255, 255)";
+})
+var Email = document.getElementById('Email');
+
+Email.addEventListener('focus',() => {
+    Email.style.borderColor = "#ccc";
+})
+
+
+var passowrd = document.getElementById('password');
+
+Email.addEventListener('focus',() => {
+    Email.style.borderColor = "#rgb(255, 255, 255)";
+})
+
+var passowrd = document.getElementById('password');
+
+Email.addEventListener('focus',() => {
+    Email.style.borderColor = "#ccc";
+})
